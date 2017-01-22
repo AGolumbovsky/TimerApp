584925c4410458986a181f52b650c45de1515c36
agolumbovsky <agolumbovsky@gmail.com>
2017-01-22 15:47:47 -0500

    finilized tests and added page title

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -34,5 +34,30 @@ describe('Countdown', () => {
                 done();
             }, 3001)
         });
+
+        it('should pause countdown on paused status', () => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(3);
+            countdown.handleStatusChange('paused');
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(3);
+                expect(countown.state.countdownStatus).toBe('paused');
+                done();
+            }, 1001);
+        });
+
+        it('should reset countdown on stopped status', () => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(3);
+            countdown.handleStatusChange('stopped');
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0);
+                expect(countown.state.countdownStatus).toBe('stopped');
+                done();
+            }, 1001);
+        });
+
     });
 });
